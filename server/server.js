c866833f884db6cb21860fc75ffcd7da40c304f0
Manojs018 <>
2026-05-14 10:00:24 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ connectDB();
 // Security & Parsing Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: true,
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
