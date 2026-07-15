8d092c7ec0b009cc8c1856b734ccc97525be297c
Manojs018 <>
2026-07-15 10:14:11 +0530

    initial commit

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getMessages, sendMessage, updateHandover } = require('../controllers/messageController');
+const { getMessages, sendMessage, updateHandover, updateDamagePins } = require('../controllers/messageController');
 const protect = require('../middleware/auth');
 
 router.get('/:bookingId', protect, getMessages);
 router.post('/:bookingId', protect, sendMessage);
 router.put('/:bookingId/handover', protect, updateHandover);
+router.put('/:bookingId/damage-pins', protect, updateDamagePins);
 
 module.exports = router;
